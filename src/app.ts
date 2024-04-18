f2d4d91041d01d72bee0c6cc9d70ae5741e4f471
unknown <SenaTarde@ARMAPRRQDFSD203.FormacionSena.RED>
2024-04-18 17:49:34 -0500

    Estructura de carpetas inicial, creacion de servidor y configuracion de envs

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,15 @@
-//app.ts
-console.log('Hola Mundo')
+import { envs } from "./config/envs"; // importar el archivo envs de config
+import { Server } from "./presentation/Server";
+
+(()=> {
+    main();
+  })()
+  
+  async function main (){
+  // todo: await base de datos
+  
+  // todo: inicio de nuestro server
+      new Server({
+        port: envs.PORT, // asignamos la variable de entorno en nuestra instancia de servidor.
+      }).start();
+  }
---
+++ src/config/envs.ts
@@ -0,0 +1,8 @@
+//importamos las librerias
+import  'dotenv/config';
+import { get } from "env-var";
+
+//creamos nuestru enum que manejara nuestra envs
+export const envs = {
+    PORT: 3000
+}
