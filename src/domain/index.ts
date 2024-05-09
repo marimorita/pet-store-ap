4c074531b65ed1ea9faad9c3059488e7c9910f0b
Mariana Méndez <marimm0927@gmail.com>
2024-05-09 17:26:51 -0500

    Implementación del AuthDatasource

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,6 @@
-//domain/index.ts
-
 export * from './dto/auth/register-user.dto';
 export * from './entities/auth/user.entity';
+
+export * from './errors/custom.error';
+
+export * from './datasources/auth.datasource'; // aqui
